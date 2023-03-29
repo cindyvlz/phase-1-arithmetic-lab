22aeb270b35dcd81b1c3ceb65d755133a2aa3746
Cindy Velasquez <ciindvlz@hotmail.com>
2023-03-29 15:18:54 -0400

    ArithLab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 9
+let num4 = 5
+const mod = num3 % num4;
+
+const max = (10, 20)
+console.log(Math.max(10, 20));
